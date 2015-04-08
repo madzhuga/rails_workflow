5b770d21a1c09f6c286ec18e0e1516fa9337a11d
Maxim Madzhuga <maximmadzhuga@gmail.com>
2015-04-08 13:56:26 +0400

    fix to avoid engine migrations to run in app.

--- lib/rails_workflow/engine.rb
+++ lib/rails_workflow/engine.rb
@@ -10,13 +10,6 @@ module RailsWorkflow
       g.helper false
     end
 
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
 
   end
 end
