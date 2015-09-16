b10f79aef77be8b5148610986a6986cce92832de
Maxim Madzhuga <maximmadzhuga@gmail.com>
2015-09-16 08:13:10 +0400

    application controller STI models fix

--- app/controllers/rails_workflow/application_controller.rb
+++ app/controllers/rails_workflow/application_controller.rb
@@ -1,11 +1,4 @@
 module RailsWorkflow
   class ApplicationController < ::ApplicationController
-    before_filter only: [:index, :show, :edit] do
-      RailsWorkflow::OperationTemplate.inheritance_column = nil
-      RailsWorkflow::Operation.inheritance_column = nil
-      RailsWorkflow::ProcessTemplate.inheritance_column = nil
-    end
-
-    # before_action :authenticate_user!
   end
 end
