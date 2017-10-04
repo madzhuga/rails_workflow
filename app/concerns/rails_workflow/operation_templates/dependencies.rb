2edf705d5aa91b35f35b6ad274b2bc9a57dd0e23
Maxim Madzhuga <maxim.madzuga@toptal.com>
2017-10-04 16:24:32 +0400

    #resolve_dependency comment update

--- app/concerns/rails_workflow/operation_templates/dependencies.rb
+++ app/concerns/rails_workflow/operation_templates/dependencies.rb
@@ -16,11 +16,15 @@ module RailsWorkflow
           )
         }
 
-        # This method can be overwritte on a specific operation templates
-        # to implement complex logic calculating if this operation should
-        # be created.
-        # TODO chech if this method needs to know completed operation or
-        # may be just a process to do necessary checks
+        # When some operation changes its status engine tries to
+        # resolve dependencies to understand if a new operation
+        # should be created.
+        # For example, if operation A depends on B (DONE) and C (DONE),
+        # by default it will be created if any of B or C changed status
+        # to DONE.
+        # Overwriting this method on some specific operation template
+        # allows you to modify this logic - for example, to build
+        # dependency B (DONE) AND C(DONE) etc.
         def resolve_dependency(_completed_operation)
           true
         end
